eaf9e9ce856379b4f48b0bf5f43d68d1ab8e29c4
Santhosh Raghav <103551648+Santhosh16R@users.noreply.github.com>
2026-04-20 14:58:04 +0530

    iteration 1

---
+++ Assets/NuitrackSDK/NuitrackDemos/All Modules/Scripts/EmergencyExit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace NuitrackSDK.NuitrackDemos
+{
+    public class EmergencyExit : MonoBehaviour
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Update()
+        {
+            //if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+        }
+    }
+}
